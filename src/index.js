34a60658830911fdd2083e8e824c74471a2b5868
ViktorRomanovsky <Romanovsky0815@gmail.com>
2023-01-22 18:51:21 +0300

    fix: mistake

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    } else {
+        return matrix
+            .map((elem, index) => {
+                return index % 2 != 0 ? elem.reverse() : elem;
+            })
+            .flat();
+    }
+};
